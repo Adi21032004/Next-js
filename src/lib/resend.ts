ff87ca6cdfb16a16e8fbcd61e5430ddc0b9707ad
Adi21032004 <nonglenadi@gmail.com>
2024-08-29 21:54:24 +0530

    check middleware

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -1,8 +1,8 @@
 import { Resend } from 'resend';
-import dotenv from 'dotenv'
-dotenv.config({
-    path: 'src/.env'
-})
+// import dotenv from 'dotenv'
+// dotenv.config({
+//     path: 'src/.env'
+// })
 
 // console.log(process.env.RESEND_API_KEY)
 export const resend = new Resend(process.env.RESEND_API_KEY);
