7ffac85cd8894d230df291baa1f3c21876221931
Adi21032004 <nonglenadi@gmail.com>
2024-08-29 21:53:16 +0530

    check middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,6 @@ import type { NextRequest } from 'next/server'
 export { default } from "next-auth/middleware" 
 import { getToken } from "next-auth/jwt"
 
-export const dynamic = 'force-dynamic'
-
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
     
