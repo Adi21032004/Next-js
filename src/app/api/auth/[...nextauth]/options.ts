6d478b410ae97872e725fb8c860bb02f40b47aef
Adi21032004 <nonglenadi@gmail.com>
2024-08-29 21:14:39 +0530

    check next auth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -7,7 +7,7 @@ import UserModel from '@/model/user';
 // dotenv.config({
 //   path: 'src/.env'
 // })
-console.log(process.env.NEXTAUTH_SECRET)
+console.log(process.env.NEXTAUTH_SECRET || 'not present')
 export const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
